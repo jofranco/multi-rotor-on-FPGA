65268e0506598e895b78051fdf9bbc1b4ec1feac
Benjamin Hobbs <bhobbs@eng.ucsd.edu>
2019-04-30 19:32:33 -0700

    updated fligt main

--- temp/PID/Vivado HLS/flightmain.cpp
+++ temp/PID/Vivado HLS/flightmain.cpp
@@ -10,52 +10,59 @@
  * ki[4] {r,p,t,y} [0,1)
  * commandOut[7] {m0,m1,m2,m3,m4,m5,kill channel} [0,1)
 */
-void flightmain (F16_t rcCmdIn[5],
-		//static F16_t measured[4],
-		F16_t obj_avd_cmd[1],
-		F16_t horizon_cmd[1],
-		F16_t obj_avd_flag[1])
+	static int run_flag=0;
+
+void flightmain (F16_t rcCmdIn[5],int obj_avd_cmd,	int horizon_cmd,int obj_avd_flag)
 {
+
 	static F16_t measured[4];
-	//avoid object
-	if (obj_avd_cmd[0]==1 && obj_avd_flag[0]==1)
+	F16_t lastrcCmdIn[5];
+	int NO_PILOT=0;
+	int PILOT_INPUT=0;
+
+	//DEFINE 'NO PILOT INPUT'
+	if((fabs(rcCmdIn[0] - (F16_t) 0.5) < 0.05 && fabs(rcCmdIn[1]-(F16_t)0.5) < 0.05
+			&& fabs(rcCmdIn[2]-(F16_t) 0.5) < 0.05 && fabs(rcCmdIn[3]-(F16_t)0.5) < 0.05))
 	{
-		rcCmdIn[0]=-measured[0];
-		rcCmdIn[1]=-measured[1];
-		rcCmdIn[2]=-measured[2];
-		rcCmdIn[3]=-measured[3];
-		rcCmdIn[4]=-measured[4];
-		//rcCmdIn[5]=-measured[5];
-
+		NO_PILOT=1;
 	}
-	//horizon
-// I TOOK OUT rcCmdIn because my test bench CONDITION makes it FAIL.
-	else if(rcCmdIn[0]==0 && rcCmdIn[1]==0 // 0 pilot
-			&& rcCmdIn[2]==0 // 0 pilot
-			&& rcCmdIn[3]==0 && rcCmdIn[4]==0   // 0 pilot
-			&& horizon_cmd[0]==1
-			&& obj_avd_cmd[0]==0)
+	//DEFINE ' PILOT INPUT'
+	if((fabs(rcCmdIn[0]-lastrcCmdIn[0])>0.1 || fabs(rcCmdIn[1]-lastrcCmdIn[1])>0.1
+		|| fabs(rcCmdIn[2]-lastrcCmdIn[2])>0.1 || fabs(rcCmdIn[3]-lastrcCmdIn[3])>0.1))
 	{
-
-		rcCmdIn[0]=0.5;
-		rcCmdIn[1]=0.5;
-		rcCmdIn[2]=0.5;
-		rcCmdIn[3]=0.5;
-		rcCmdIn[4]=0.5;
-		//rcCmdIn[5]=0.5;
+		PILOT_INPUT=1;
 	}
-	// rc
-
-	else if(rcCmdIn[0]!=0 || rcCmdIn[1]!=0 // 1 pilot
-			|| rcCmdIn[2]!=0 // 1 pilot
-			|| rcCmdIn[3]!=0 || rcCmdIn[4]!=0 // 1 pilot
-			&& obj_avd_cmd[0]==0)
+	//FM avoid object
+	if (obj_avd_cmd==1 && obj_avd_flag==1)
 	{
-	//just do whatever Rc says.... dont think I actually need this state.
+		//remember that in our code....0.1 is OFF? and 0.99 is forwards....? 0.5 is stationary
+		rcCmdIn[0]=round( (F16_t)0.99- measured[0]); //roll
+		rcCmdIn[1]= round((F16_t)0.99 - measured[1]); //pitch
+
+		rcCmdIn[2]= /*0.9 - */ measured[2]; //not sure this is needed.
+		rcCmdIn[3]= round( (F16_t)0.99- measured[3]); //yaw
 	}
-
-
-
+	//FM horizon
+	else if( (obj_avd_cmd==0 && NO_PILOT==1 && horizon_cmd==1) ||
+			( NO_PILOT==1 && obj_avd_flag==0 &&horizon_cmd==1))
+	{
+		rcCmdIn[0]=0.5; //roll 	->THIS DOESN"T NEED TO BE HERE IF THE STICKS ARE "CENTERED"
+		rcCmdIn[1]=0.5; //pitch ->BUT FOR THE TEST BENCH WE HAVE NO "STICKS"
+	}
+	//FM acro
+	else if( (  obj_avd_cmd==0 && PILOT_INPUT==1 ) || (horizon_cmd==0 &&  obj_avd_flag==0 ) ||
+			(horizon_cmd=0 && obj_avd_cmd==0) )
+		{
+			rcCmdIn[0]=lastrcCmdIn[0];
+			rcCmdIn[1]=lastrcCmdIn[1];
+			rcCmdIn[2]=lastrcCmdIn[2];
+			rcCmdIn[3]=lastrcCmdIn[3];
+		}
+	 lastrcCmdIn[0]=rcCmdIn[0];
+	 lastrcCmdIn[1]=rcCmdIn[1];
+	 lastrcCmdIn[2]=rcCmdIn[2];
+	 lastrcCmdIn[3]=rcCmdIn[3];
+	 run_flag=1;
 }
 
 
--- temp/PID/Vivado HLS/test.cpp
+++ temp/PID/Vivado HLS/test.cpp
@@ -8,11 +8,13 @@ int main(){
 	// expected pitch and roll values.
 	// then you can graph both PID's of pitch/roll
 	//{r,p,t,y}
-	F16_t obj_avd_cmd[0]=0;
-	F16_t horizon_cmd[0]=1;
-	F16_t obj_avd_flag[0]=0;
+	int obj_avd_cmd =0;
+	int horizon_cmd=0;
+	int obj_avd_flag=1;
 
-	F16_t rcCmdIn[5]= {0.0,0.0,0.0,0.0,0.0};
+	F16_t rcCmdIn[5]= {0.7 ,0.5 ,0.5 ,0.5,/*kill*/0.0};
+	F16_t lastrcCmdIn[5]= {0.0 ,0.0 ,0.0 ,0.0,/*kill*/0.0};
+
 	F16_t measured[4]={0.1,0.8,0.0,0.0};
 	F32_t kp[3]={0.5,0.5,0.5};
 	F32_t kd[3]={0.1,.1,.1};
@@ -23,6 +25,7 @@ int main(){
 	F19_t p_c=0;
 	F19_t y_c=0;
 
+
 	/** pid
 	 * rcCmdIn[4] {r,p,t,y} [0,1) * measured[4] {r,p,t,y} [0,1)
 	 * dt [0,1)
@@ -32,22 +35,24 @@ int main(){
 	 * commandOut[7] {m0,m1,m2,m3,m4,m5,kill channel} [0,1)
 	*/
 
- flightmain( rcCmdIn, obj_avd_cmd,horizon_cmd, obj_avd_flag);
-
-for( int i =1; i<=10; i++){
-	printf("HORIZON COMMAND %g |",float(horizon_cmd[0]));
-	printf("%g,  ", float(measured[0]));//R,P,T,Y
-	printf("%g; \n ", float(measured[1]));
+ 	for( int i =1; i<=10; i++){
+ 	printf("INPUTS to flightmain %g |", float(fabs(rcCmdIn[0] - (F16_t) 0.5)));
+	printf("HORIZON COMMAND %g |",float(horizon_cmd));
+	printf("rcCmdIn %g |",float(rcCmdIn[0]));
+	printf("ROLL %g |,  ", float(measured[0]));//R,P,T,Y
+	printf(" PITCH %g |; \n ", float(measured[1]));
+	flightmain( rcCmdIn, obj_avd_cmd, horizon_cmd, obj_avd_flag);
 
 	pid(rcCmdIn,measured,kp,kd, ki,commandOut);//
+
 		/*
 		printf( " %g, ", float(commandOut[1])); //p
 		printf( " %g, ", float(commandOut[2])); //t....thrust=rcCmdin...no PID
 		printf( " %g; \n ", float(commandOut[3]));//y
 		*/
+
 		measured[0]= measured[0]+commandOut[0];
 		measured[1]= measured[1]+commandOut[1];
-
 }
 
 	return 0;
