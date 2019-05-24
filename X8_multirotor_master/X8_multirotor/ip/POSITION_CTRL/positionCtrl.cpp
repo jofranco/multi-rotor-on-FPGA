61180bdd649dc3bf8d57b507190272295fc5c133
Jeffrey Franco <jfranco519@yahoo.com>
2019-05-24 14:31:44 -0700

    Updating files for positionCtrl. Adding new files for filter

--- X8_multirotor_master/X8_multirotor/ip/POSITION_CTRL/positionCtrl.cpp
+++ X8_multirotor_master/X8_multirotor/ip/POSITION_CTRL/positionCtrl.cpp
@@ -3,13 +3,109 @@
 #include "ap_int.h"
 #include "stdint.h"
 #include "positionCtrl.hpp"
+#include "filter.hpp"
 
-/** positionCtrl
- * pmod_data[SIZE] - raw data from pmod
- * pos_data[6] {roll, pitch, yaw, rollRate, pitchRate, yawRate}
+/**
+ * 
+ * float Nav_GetGRangeLSB(uint8_t bRangeG)
+ * 		Converts the value from the 16 bits reading to the float value (in g)
+ *	    corresponding to the acceleration, considering the current selected g
+ *      range.
+ * 
+ * @param bRangeG - the parameter specifying the dps range. Can be one of the
+ *                	parameters from the following list:
+ *                   0   NAV_GYRO_PAR_G_245DPS  Parameter dps range : +/- 245dps
+ *                   1   NAV_GYRO_PAR_G_500DPS  Parameter dps range : +/- 500dps
+ *                   3   NAV_GYRO_PAR_G_2kDPS   Parameter dps range : +/- 2kdps
+ * 
+ * @return float - the corresponding value of one LSB unit according to the range set
+*/
+float Nav_GetGRangeLSB(uint8_t bRangeG) 
+{
+    float gRangeLSB;
+    //pyprintf("Executing NAV_GetGRangeLSB..\n");
+    switch (bRangeG) 
+    {
+    case NAV_GYRO_PAR_G_245DPS:
+        gRangeLSB = 0.00875;
+        //pyprintf("Selecting gRangeLSB: %f\n", gRangeLSB);
+            break;
+    case NAV_GYRO_PAR_G_500DPS:
+        gRangeLSB = 0.0175;
+            break;
+    case NAV_GYRO_PAR_G_2kDPS:
+        gRangeLSB = 0.07;
+            break;
+    default:
+        gRangeLSB = 0.00875;
+        break;
+   }
+    //pyprintf("Exiting NAV_GetGRangeLSB...\n");
+   return gRangeLSB;
+}
+
+/**
+ * 
+ * float Nav_GetXLRangeLSB(uint8_t bRangeXL)
+ * 		The function computes the range LSB based on the set range parameter.
+ *      The argument values are between 0 and 3. If the argument is within the
+ *      accepted values range, it selects the range LSB value for further
+ *      computations of the "g" value. If value is outside this range, the
+ *      default value is set.
+ * 
+ * @param bRangeXL - the parameter specifying the g range. Can be one of the
+ *                 	 parameters from the following list:
+ *                    0   NAV_ACL_PAR_XL_2G   Parameter g range : +/- 2g
+ *                    1   NAV_ACL_PAR_XL_4G   Parameter g range : +/- 4g
+ *                    2   NAV_ACL_PAR_XL_8G   Parameter g range : +/- 8g
+ *                    3   NAV_ACL_PAR_XL_16G  Parameter g range : +/- 16g
+ * 
+ * @return float - the corresponding value of one LSB unit according to the range set
+ * 
+ */
+float Nav_GetXLRangeLSB(uint8_t bRangeXL) 
+{
+    float xlRangeLSB;
+    //pyprintf("Executing Nav_GetXLRangeLSB..\n");
+    switch (bRangeXL) 
+    {
+    case NAV_ACL_PAR_XL_2G:
+      xlRangeLSB = 0.000061;
+      break;
+   case NAV_ACL_PAR_XL_4G:
+      xlRangeLSB = 0.000122;
+      break;
+   case NAV_ACL_PAR_XL_8G:
+      xlRangeLSB = 0.000244;
+      break;
+   case NAV_ACL_PAR_XL_16G:
+      xlRangeLSB = 0.000732;
+      break;
+   default:
+      xlRangeLSB = 0.000061;
+      break;
+   }
+    //pyprintf("Exiting NAV_GetXLRangeLSB...\n");
+   return xlRangeLSB;
+}
+
+/** 
+ * 
+ * void positionCtrl(uint32_t pmod_data[SIZE], uint16_t pos_data[SIZE])
+ * 		Receives the following raw data from pmod nav:
+ * 			-Accelerometer Data (x,y,z)
+ * 			-Gyroscope Data (x,y,z)
+ * 			The raw data comes from the custom spi driver.
+ * 
+ * @param	pmod_data[SIZE] - raw data from pmod
+ * @param	pos_data[6] {roll, pitch, yaw, rollRate, pitchRate, yawRate}
+ * 
+ * @return	None.
+ * 
+ * @note	None.
  */
 
-void positionCtrl(uint16_t pmod_data[SIZE], uint16_t pos_data[SIZE])
+void positionCtrl(uint32_t pmod_data[SIZE], uint16_t pos_data[SIZE])
 {
 	// HLS PRAGMAS
 	#pragma HLS PIPELINE II=1 enable_flush
--- X8_multirotor_master/X8_multirotor/ip/POSITION_CTRL/positionCtrl.hpp
+++ X8_multirotor_master/X8_multirotor/ip/POSITION_CTRL/positionCtrl.hpp
@@ -5,4 +5,20 @@
 
 #define SIZE 4096
 
+//Parameter dps range : +/- 245dps
+#define NAV_GYRO_PAR_G_245DPS 0
+//Parameter dps range : +/- 500dps
+#define NAV_GYRO_PAR_G_500DPS 1
+//Parameter dps range : +/- 2kdps
+#define NAV_GYRO_PAR_G_2kDPS 3
+
+//Parameter G range : +/- 2g
+#define NAV_ACL_PAR_XL_2G 0
+//Parameter G range : +/- 4g
+#define NAV_ACL_PAR_XL_4G 2 
+//Parameter G range : +/- 8g
+#define NAV_ACL_PAR_XL_8G 3
+//Parameter G range : +/- 16g
+#define NAV_ACL_PAR_XL_16G 1
+
 void positionCtrl(uint16_t pmod_data[SIZE], uint16_t pos_data[SIZE]);
