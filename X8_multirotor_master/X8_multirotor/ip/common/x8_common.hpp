24118e01b86b825dfdd0262c326fd587f44721e1
Jeffrey Franco <jfranco519@yahoo.com>
2019-05-27 15:19:51 -0700

    adding comments

--- X8_multirotor_master/X8_multirotor/ip/common/x8_common.hpp
+++ X8_multirotor_master/X8_multirotor/ip/common/x8_common.hpp
@@ -1,6 +1,6 @@
 //include libraries
-#ifndef X8_COMMON_HPP
-#define X8_COMMON_HPP
+#ifndef X8_COMMON_HPP /* prevent circular inclusions */
+#define X8_COMMON_HPP /* by using protection macros */
 
 
 #include "ap_fixed.h"
@@ -48,4 +48,4 @@ typedef enum
 // scales raw RC channel data to [0:1)
 uint16_t scaleRange(uint16_t x, uint16_t srcFrom, uint16_t srcTo, uint16_t destFrom, uint16_t destTo);
 
-#endif
+#endif /* end of protection macro, POSITIONCTRL_HPP */
