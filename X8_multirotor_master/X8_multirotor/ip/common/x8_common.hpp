f118cafa12e0f84cd76f5f6fa7c1569579ef210a
Aaron <adcoffma@eng.ucsd.edu>
2019-05-27 20:02:36 -0700

    updating x8_common.hpp F16_t<16,3>

--- X8_multirotor_master/X8_multirotor/ip/common/x8_common.hpp
+++ X8_multirotor_master/X8_multirotor/ip/common/x8_common.hpp
@@ -28,7 +28,7 @@ typedef ap_fixed<128,96> F128_t;
 typedef ap_fixed<64,32> F64_t;
 typedef ap_fixed<32, 16> F32_t;
 typedef ap_fixed<19, 4> F19_t;
-typedef ap_fixed<16,2> F16_t;
+typedef ap_fixed<16,3> F16_t;
 
 typedef ap_uint<6> uint6_t;
 
